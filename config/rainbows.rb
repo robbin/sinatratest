071de7ff7f956ad5b4d950ca5ad7f80e5c8a4c32
robbin <fankai@gmail.com>
2013-01-06 01:03:37 +0800

    test rainbows

--- config/rainbows.rb
+++ config/rainbows.rb
@@ -1,7 +1,5 @@
-worker_processes 1 # assuming four CPU cores
+worker_processes 2 # assuming four CPU cores
 Rainbows! do
-  # use :ThreadSpawn
   use :ThreadPool
-  # use :XEpollThreadPool, :pool_size => 50
-  worker_connections 5
+  worker_connections 16
 end
